8f870b3cba3545160b7a14689102921cd674f1e5
spacer730 <hoeshashemi_199@msn.com>
2019-08-21 18:42:41 +0200

    Experimented with datashader, added UMAP plots of full COSMOS2015 dataset for small scatter sizes and added results for estimators/regressors for SSFR/age/zphoto

--- UMAP-Algorithm-Regressor_COSMOS2015.py
+++ UMAP-Algorithm-Regressor_COSMOS2015.py
@@ -5,9 +5,15 @@ import os
 import umap
 import matplotlib.colors
 import matplotlib as mpl
+#import datashader as ds
 from astropy.io import fits
 from astropy.table import Table
+#from colorcet import fire
+#from datashader import transfer_functions as tf
+from matplotlib import cm
+#from datashader.bokeh_ext import InteractiveImage
 
+#import bokeh.plotting as bp
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor, RadiusNeighborsRegressor
 from sklearn.svm import SVR
@@ -58,51 +64,76 @@ def rmse(predictions, targets):
     return np.sqrt(((predictions - targets) ** 2).mean())
 
 #Training knn regressor for redshift on both the embedded data as well as the original data
-neigh = [KNeighborsRegressor(n_neighbors=i, weights='distance').fit(train_embedding_bin, mass_train_bin) for i in range(1,100)]
-mass_test_pred = [neigh[i].predict(test_embedding_bin) for i in range(len(neigh))]
-score_pred = [neigh[i].score(test_embedding_bin, mass_test_bin) for i in range(len(neigh))]
-rmse_pred = [rmse(mass_test_pred[i], mass_test_bin) for i in range(len(mass_test_pred))]
+neigh = [KNeighborsRegressor(n_neighbors=i, weights='distance').fit(train_embedding_bin, zphoto_train_bin) for i in range(1,100)]
+zphoto_test_pred = [neigh[i].predict(test_embedding_bin) for i in range(len(neigh))]
+score_pred = [neigh[i].score(test_embedding_bin, zphoto_test_bin) for i in range(len(neigh))]
+rmse_pred = [rmse(zphoto_test_pred[i], zphoto_test_bin) for i in range(len(zphoto_test_pred))]
 
-neigh2 = [KNeighborsRegressor(n_neighbors=i, weights='distance').fit(X_train_bin, mass_train_bin) for i in range(1,15)]
-mass_test_pred_2 = [neigh2[i].predict(X_test_bin) for i in range(len(neigh2))]
-score_pred_2 = [neigh2[i].score(X_test_bin, mass_test_bin) for i in range(len(neigh2))]
-rmse_pred_2 = [rmse(mass_test_pred_2[i], mass_test_bin) for i in range(len(mass_test_pred_2))]
+neigh2 = [KNeighborsRegressor(n_neighbors=i, weights='distance').fit(X_train_bin, zphoto_train_bin) for i in range(1,15)]
+zphoto_test_pred_2 = [neigh2[i].predict(X_test_bin) for i in range(len(neigh2))]
+score_pred_2 = [neigh2[i].score(X_test_bin, zphoto_test_bin) for i in range(len(neigh2))]
+rmse_pred_2 = [rmse(zphoto_test_pred_2[i], zphoto_test_bin) for i in range(len(zphoto_test_pred_2))]
 
 """
-plt.plot([i+1 for i in range(len(rmse_pred_2))], rmse_pred_2)
+plt.plot([i+1 for i in range(len(rmse_pred))], rmse_pred)
 plt.xlabel('# nearest neighbors')
-plt.ylabel('RMSE')
-plt.text(12, 0.21, 'mass')
+plt.ylabel('rmse')
+plt.text(80, 0.060, 'zphoto')
 plt.show()
 """
 
 #Training support vector regressor for redshift on both the embedded data as well as the original data
-svr1 = SVR().fit(train_embedding_bin, mass_train_bin)
-mass_test_pred_3 = svr1.predict(test_embedding_bin)
-score_pred_3 = svr1.score(test_embedding_bin, mass_test_bin)
-rmse_pred_3 = rmse(mass_test_pred_3, mass_test_bin)
+svr1 = SVR().fit(train_embedding_bin, zphoto_train_bin)
+zphoto_test_pred_3 = svr1.predict(test_embedding_bin)
+score_pred_3 = svr1.score(test_embedding_bin, zphoto_test_bin)
+rmse_pred_3 = rmse(zphoto_test_pred_3, zphoto_test_bin)
 
-svr2 = SVR().fit(X_train_bin, mass_train_bin)
-mass_test_pred_4 = svr2.predict(X_test_bin)
-score_pred_4 = svr2.score(X_test_bin, mass_test_bin)
-rmse_pred_4 = rmse(mass_test_pred_4, mass_test_bin)
+svr2 = SVR().fit(X_train_bin, zphoto_train_bin)
+zphoto_test_pred_4 = svr2.predict(X_test_bin)
+score_pred_4 = svr2.score(X_test_bin, zphoto_test_bin)
+rmse_pred_4 = rmse(zphoto_test_pred_4, zphoto_test_bin)
+
+"""
+#All the code of my attempts to make nicer plots with datashader
+mass_train_2 = np.array([[i] for i in mass_train])
+data_array = np.hstack((train_embedding, mass_train_2))
+df = pd.DataFrame(data=data_array, index=np.array([i for i in range(len(data_array))]), columns=np.array(['x','y','mass']))
+
+agg = ds.Canvas(plot_width=500, plot_height=500, x_range=(-6.5,10.25), y_range=(-10.5,10.5), x_axis_type='linear', y_axis_type='linear').points(df,'x','y', agg=ds.sum('mass'))
+img = tf.set_background(tf.shade(agg, cmap=cm.get_cmap('autumn_r')),"black")
+ds.utils.export_image(img=img,filename='Test4', fmt=".png", background="black")
+
+def image_callback(x_range, y_range, w, h, name=None):
+    cvs = ds.Canvas(plot_width=w, plot_height=h, x_range=x_range, y_range=y_range)
+    agg = cvs.points(df, 'x', 'y', ds.sum('mass'))
+    img = tf.shade(agg, cmap=cm.get_cmap('summer'))
+    return tf.dynspread(img, threshold=0.50, name=name)
+
+imgs = image_callback((-6.5,10.25), (-10.5,10.5), 500, 500, name="Original")
+ds.utils.export_image(img=imgs,filename='Test5', fmt=".png", background="black")
+
+bp.output_notebook()
+p = bp.figure(tools='pan,wheel_zoom,reset', x_range=(-6.5,10.25), y_range=(-10.5,10.5), plot_width=500, plot_height=500)
+
+InteractiveImage(p, image_callback)
+"""
 
 #Split the dataset into two different groups
-split = 9#np.mean(age_train)+np.var(age_train)**0.5
-split_a = mass_test_pred_2[9]<split
-split_b = mass_test_pred_2[9]>=split
+split = 3*10**9#np.mean(age_train)+np.var(age_train)**0.5
+split_a = age_train<split
+split_b = age_train>=split
 
 #visualize the distribution of galaxies in the compressed feature space
 fig, axs = plt.subplots()
 
 #x,y coordinates and the size of the dot and whether to use a logscale for the colors
-#CSa = axs.scatter(test_embedding_bin[:, 0][split_a], test_embedding_bin[:, 1][split_a], 3, c=mass_test_pred_2[9][split_a], cmap='summer', norm=matplotlib.colors.Normalize(vmin=7,vmax=9))
-#cbara = fig.colorbar(CSa)
-axs.text(5,6,'0.7<=z<0.9',color='k')
-CSb = axs.scatter(test_embedding_bin[:, 0][split_b], test_embedding_bin[:, 1][split_b], 3, c=mass_test_pred_2[9][split_b], cmap='autumn_r', norm=matplotlib.colors.Normalize(vmin=9,vmax=12))
+CSa = axs.scatter(train_embedding[:, 0][split_a], train_embedding[:, 1][split_a], 0.01, c=age_train[split_a], cmap='summer', norm=matplotlib.colors.LogNorm(vmin=np.min(age_train),vmax=split))
+cbara = fig.colorbar(CSa)
+#axs.text(5,6,'0.7<=z<0.9',color='k')
+CSb = axs.scatter(train_embedding[:, 0][split_b], train_embedding[:, 1][split_b], 0.01, c=age_train[split_b], cmap='autumn_r', norm=matplotlib.colors.LogNorm(vmin=split,vmax=np.max(age_train)))
 cbarb = fig.colorbar(CSb)
-#cbara.set_label('mass<9')
-cbarb.set_label('9<=mass')
-axs.set_xlim([-6.5,10.25])
-axs.set_ylim([-10.5,10.5])
+cbara.set_label('age<3.0 Gyr')
+cbarb.set_label('3.0 Gyr<=age')
+#axs.set_xlim([-6.5,10.25])
+#axs.set_ylim([-10.5,10.5])
 plt.show()
